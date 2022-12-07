d8977cb0de790bb64716a49fbd96aad28000dfaf
Johannes Stahr <johannesstahrofficial@gmail.com>
2022-12-07 23:16:03 +0100

    start js

---
+++ Webentwicklung test/domapi.js
@@ -0,0 +1,23 @@
+const person_array = [
+    {
+        firstname: "lenny",
+        lastname: "lindemann",
+        gender:"male"
+    },
+    {
+        firstname:"Luca",
+        lastname:"Tuchti",
+        gender:"male"
+    },
+    {
+        firstname:"Jakob",
+        lastname:"Gäbler",
+        gender:"female"
+    }
+]
+
+function create_tabel(person){
+    const container = document.getElementById("create_tabel")
+    
+
+}
