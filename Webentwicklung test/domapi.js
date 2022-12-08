c7945e598f0dbf885971f09cba63252660ec3fb8
Johannes Stahr <johannesstahrofficial@gmail.com>
2022-12-08 22:00:20 +0100

    input data from js object

--- Webentwicklung test/domapi.js
+++ Webentwicklung test/domapi.js
@@ -1,23 +1,52 @@
-const person_array = [
-    {
-        firstname: "lenny",
-        lastname: "lindemann",
-        gender:"male"
-    },
-    {
-        firstname:"Luca",
-        lastname:"Tuchti",
-        gender:"male"
-    },
-    {
-        firstname:"Jakob",
-        lastname:"Gäbler",
-        gender:"female"
-    }
-]
+function tryjs() {
+  testcreateelement();
+  create_tabel(persons);
+}
 
-function create_tabel(person){
-    const container = document.getElementById("create_tabel")
-    
+const persons = [
+  {
+    firstname: "lenny",
+    lastname: "lindemann",
+    gender: "male",
+  },
+  {
+    firstname: "Luca",
+    lastname: "Tuchti",
+    gender: "male",
+  },
+  {
+    firstname: "Jakob",
+    lastname: "Gäbler",
+    gender: "female",
+  },
+];
 
+function create_tabel(persons) {
+  const container = document.getElementById("create_tabel");
+  const tabel = document.createElement("tabel");
+  const thead = document.createElement("thead");
+  thead.innerHTML =
+    "<tr> <th> Vorname </th> <th> Nachname </th> <th> Position </th> </tr>";
+
+  const tbody = document.createElement("tbody");
+
+  for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    const tr = document.createElement("tr");
+    tr.innerHTML =
+      `<td> ${person.firstname} </td> <td> ${person.lastname} </td> <td> ${person.gender} </td>`;
+    tbody.appendChild(tr);
+  }
+
+  tabel.appendChild(thead);
+  tabel.appendChild(tbody);
+  container.appendChild(tabel);
+}
+
+function testcreateelement() {
+  const container = document.getElementById("create_tabel");
+
+  const btn = document.createElement("button");
+  btn.innerHTML = "clickme";
+  container.appendChild(btn);
 }
