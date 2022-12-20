e3485fb910bcaf3c1669f45efdd775fb0451e359
Johannes Stahr <johannesstahrofficial@gmail.com>
2022-12-21 00:42:29 +0100

    display api data in html

--- js_concepts/fetch.js
+++ js_concepts/fetch.js
@@ -2,29 +2,26 @@ const btn = document.getElementById("search-input");
 
 btn.addEventListener("click", getDatafromUrl());
 
-const api_data = {};
-
 function getDatafromUrl() {
-  fetch("https://randomuser.me/api/?results=2")
+  fetch("https://randomuser.me/api/?results=10")
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
       console.log(data);
-    })
-    /*.then(function (data) {
-      data.forEach((user) => {
-        const userinfo = document.createElement("li");
-        userinfo.innerHTML(`${user.email}`);
 
+      // loop through the array of users
+      data.results.forEach((user) => {
+        // create an li element for each user
+        const userinfo = document.createElement("li");
+        // set the innerHTML of the li to the user's email address
+        userinfo.innerHTML = user.email +' '+ user.location.country;
+
+        // append the li to the ul element with the id "userinfo"
         document.getElementById("userinfo").appendChild(userinfo);
       });
-    })*/
+    })
     .catch(function (err) {
       console.log("Fetch Error :-S", err);
     });
 }
-
-let outputdata = () => {
-  console.log(api_data);
-};
