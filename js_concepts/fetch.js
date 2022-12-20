cf060ca3a6caa26700db9cbb3637b22736dbd7d5
Johannes Stahr <johannesstahrofficial@gmail.com>
2022-12-20 23:41:37 +0100

    change fetches in js

--- js_concepts/fetch.js
+++ js_concepts/fetch.js
@@ -5,13 +5,21 @@ btn.addEventListener("click", getDatafromUrl());
 const api_data = {};
 
 function getDatafromUrl() {
-  fetch("https://randomuser.me/api/")
+  fetch("https://randomuser.me/api/?results=5000")
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-      console.log(data)
+      console.log(data);
     })
+    /*.then(function (data) {
+      data.forEach((user) => {
+        const userinfo = document.createElement("li");
+        userinfo.innerHTML(`${user.email}`);
+
+        document.getElementById("userinfo").appendChild(userinfo);
+      });
+    })*/
     .catch(function (err) {
       console.log("Fetch Error :-S", err);
     });
