2fadad04be141c9ffed969aa638b3c8fc087457f
xccsk <johannesstahrofficial@gmail.com>
2022-12-29 18:50:10 +0100

    update js playground

---
+++ jsbook/wiederverwendbarecode/test_of_arguments.js
@@ -0,0 +1,13 @@
+const testfunction = function lennywasnlos(age, name, lastname) {
+  if (age !== undefined) {
+    console.log(age);
+  }
+  if (name !== undefined) {
+    console.log(name);
+  }
+  if (lastname !== undefined) {
+    console.log(lastname);
+  }
+};
+
+
