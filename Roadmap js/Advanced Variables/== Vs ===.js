728471ebfe23e95769d9eeb2b89c0741a92e6eae
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-02-26 17:44:18 +0100

    == vs ===

---
+++ Roadmap js/Advanced Variables/== Vs ===.js
@@ -0,0 +1,21 @@
+// double and Tripple equals 
+
+console.log(1==1) // true
+
+console.log(1=="1") //true
+
+//double equalsigne converts the type of the object to the same 
+
+console.log(1 === "1") //false (couse it compares the type of the object)
+
+console.log(0=="") //true couse it converts the type of the object
+
+console.log(0==false) //true
+
+console.log(null == null) // true
+
+console.log(null === undefined) //false
+
+console.log(null == undefined)//true
+
+/* 3x = compares the the objcet type and == converts the valies to the same objects */
