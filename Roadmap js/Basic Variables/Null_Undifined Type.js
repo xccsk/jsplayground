334c4608fe231b4aa4ffae13458b2c9534b3c180
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-02-26 18:02:29 +0100

    get null and undifined

---
+++ Roadmap js/Basic Variables/Null_Undifined Type.js
@@ -0,0 +1,14 @@
+// null and undifined 
+
+/*
+Similarities: null and unfinied both mean there is no value
+
+null: null will not popup in the programm for an error or something this only returns when its set by a programmer. null return when there is no value but it exists
+
+undifinded: undifind is the default state of everything if let a and console.log(a) then you get undifined becouse there is nothing in it and its basicly not clear if there is gonna be a defined value or not so if its existing or not 
+*/
+
+let a 
+
+console.log(a) //undifined
+
