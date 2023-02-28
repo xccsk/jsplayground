d043ed6b0d6f9d8f84084e65b668b6b183776487
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-02-28 18:52:47 +0100

    ternary operator

---
+++ Roadmap js/Control Flow.js/Ternary Operator.js
@@ -0,0 +1,22 @@
+//Ternary Operator
+
+//it tests a condition ? and its a shortform of a if statement
+
+true ? "yes" : "no";
+
+//its for seting values for a contion thats turety or falsy 
+
+//you can also make bigger expressions
+
+(
+    //some operation thats true or false
+    age > height
+) ? (
+    console.log("hello")
+    //any other code
+)
+:
+(
+    console.log(false)
+
+)
