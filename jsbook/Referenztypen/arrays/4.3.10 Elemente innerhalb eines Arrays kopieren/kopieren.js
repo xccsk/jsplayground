0ca5252e38358b9c3658737541ef99d26a1f95dc
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-02 19:44:02 +0100

    array coping and array to sting

---
+++ jsbook/Referenztypen/arrays/4.3.10 Elemente innerhalb eines Arrays kopieren/kopieren.js
@@ -0,0 +1,11 @@
+// die copywithin method
+
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+
+/*Diese Methode kann bis zu 3 argumente nehmen 
+1. den index an den die Elemente kopiert werden sollen,
+2. den index ab dem die Elemente kopiert werden und
+3. den Index bis wohin die Elemnete kompiert werden
+*/
+
+array.copyWithin(0,2,4) // an den anfagn des arrays werden die Zahlen 3,4,5 angeganden
---
+++ jsbook/Referenztypen/arrays/4.3.11 arrays in zeichenketten umwandeln/toString.js
@@ -0,0 +1,10 @@
+// arrays in strings verwandeln
+
+// 3 möglichkeiten toSting(), toLocalSting(), valueOf()
+
+ //alternativ auch .join() (damit kann man alle emlemtne eines arrays in einer zeichenkette speichern, und auch gewünscht abgrenzen .join("-"))
+
+ array=['lenny','max','john']
+
+ array.toString()
+ array.toLocaleString()
