8969a7d7a7eb6a598837432a19ce7f5d961de27a
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-04 07:51:25 +0100

    if condtions in js

---
+++ Roadmap js/Control Flow.js/if.js
@@ -0,0 +1,24 @@
+// in JS you use if statments to run some certain code block 
+
+if (condition == true) {
+    //run this code
+}
+
+//now we maybe want to ececute a diffrent block if its not true
+
+if (condition) {
+    // code to execute if condition is true
+  } else {
+    // code to execute if condition is false
+  }
+  
+//Sometimes we may have more than two possible outcomes based on the condition we're testing. For this, we can use "else if" statements. Here's the syntax:
+
+if (condition1) {
+    // code to execute if condition1 is true
+  } else if (condition2) {
+    // code to execute if condition2 is true
+  } else {
+    // code to execute if all conditions are false
+  }
+  
