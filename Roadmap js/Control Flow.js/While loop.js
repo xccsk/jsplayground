4a9194e786023b1f4d2a9f3939f45c5678dbe194
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-04 08:12:18 +0100

    while loop and do while loop

---
+++ Roadmap js/Control Flow.js/While loop.js
@@ -0,0 +1,19 @@
+// While loop
+
+/* 
+In JavaScript, a "while" loop is used to repeatedly execute a block of code while a certain condition is true. Here's the basic syntax for a "while" loop:
+*/
+
+while (condition) {
+  // code to execute while condition is true
+}
+
+//do while
+
+/*
+a "do...while" loop will always execute the code inside the curly braces at least once, regardless of whether the condition is true or false.
+ */
+
+do {
+  // code to execute at least once
+} while (condition);
