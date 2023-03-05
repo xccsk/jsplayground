fb80f128d18c94f81e080de55b0812cb1fb2ef7e
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-05 16:59:53 +0100

    remove api key

---
+++ jsbook/Referenztypen/Reguläre ausdrücke/Introduction.js
@@ -0,0 +1,32 @@
+//regular expression
+
+/*they are there to make sure that there is a test if there are certain charakters in a string or text */
+
+//there are 6 things methoeds for regular expressions
+
+const test = "igfuufhdiuhfiuh";
+
+test.replace(); // ersetzt buchstaben oder ähnliches mit den angegebenen werden
+
+test.test()
+
+test.exec()
+
+test.match()
+
+test.search()
+
+test.split()
+
+//initalizing a regular via kontruktrue functiuon
+
+const regExpre = new RegExp('lenny')
+
+//literal writing
+
+const RegExpress= "lenny" // so just a string
+
+//the test method
+
+const inclues = RegExpress.test('lenny8hwf8hwuchuwhcwc') //this returns true couse it incloudes the RegExpress
+   
