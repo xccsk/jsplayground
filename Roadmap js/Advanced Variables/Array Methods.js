4a0933815dc4fc41c4ede41d0d299ed15f93bee1
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-06 21:05:06 +0100

    first array methods

---
+++ Roadmap js/Advanced Variables/Array Methods.js
@@ -0,0 +1,27 @@
+//the most common methods in arrays
+
+const array = [1,2,3,4,"d","g","l","M"]
+
+//push(): Fügt ein oder mehrere Elemente am Ende des Arrays hinzu.
+
+array.push(1) //[1,2,3,4,"d","g","l","M",1]
+
+//pop(): Entfernt das letzte Element aus dem Array.
+
+array.pop() //[1,2,3,4,"d","g","l"] 
+
+//shift(): Entfernt das erste Element aus dem Array.
+
+array.shift() //[2,3,4,"d","g","l","M"]
+
+//unshift(): Fügt ein oder mehrere Elemente am Anfang des Arrays hinzu.
+
+array.unshift() //fast das selbe wie push
+
+//slice(): Erstellt eine Kopie eines Teils des Arrays.
+
+array.slice() // anhand der parameter gibst du an start anzahl und endpunkt
+
+//splice(): Fügt oder entfernt Elemente an oder von einer bestimmten Position im Array.
+
+array.splice() //entfernt und 
