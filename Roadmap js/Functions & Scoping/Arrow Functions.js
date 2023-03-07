c3ca036b87a6328a3b2e010193aecea34a05f6ca
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-07 10:25:23 +0100

    arrow functions

---
+++ Roadmap js/Functions & Scoping/Arrow Functions.js
@@ -0,0 +1,41 @@
+// Arrow functions
+
+//1. converting normal funktions in to JS funktions
+
+function sum(a, b) {
+  return a + b;
+}
+
+function isPositive(number) {
+  return number >= 0;
+}
+
+function randomNumber() {
+  return Math.random;
+}
+
+document.addEventListener("click", function () {
+  console.log("Click");
+});
+
+//arrows
+
+const sum2 = (a, b) => {
+  return a + b;
+};
+
+const isPositive2 = number => {
+  return number >= 0;
+};
+
+const randomNumber2 = () => Math.random
+
+document.addEventListener("click", ()=>{console.log()})
+
+
+//2. Diffrence between this in Arrow and normal functions
+
+/*when using this ina normal function it looks on the scope from where the function is call.
+
+When using arrow functions it looks up the ths keyword from where its defined
+*/
