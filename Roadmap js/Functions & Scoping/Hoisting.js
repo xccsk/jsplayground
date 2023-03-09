b4d4625e001f3a7aa000a7de2545a69e05785b5e
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-09 10:04:53 +0100

    Hoisting explaining

---
+++ Roadmap js/Functions & Scoping/Hoisting.js
@@ -0,0 +1,34 @@
+// Hoisting
+
+/*Hoisting takes some of the code and moves it on the top of the file for us 
+
+that only works for normal functions with a function keyword and not for arrow-functions
+
+varibales like const and let are not hoisted
+
+but var variables are hoisted but not with the value its just getting initalized and then its gonna be undefined until its defined in the line 
+*/
+
+//function example
+
+const submit = ()=>{
+    return 14
+}
+
+console.log(submit)
+console.log(addition)
+
+function addition(){
+    return 14+11
+}
+
+//in this code wont be an error, cause its gonne hoist the funcionnaddtion up if submit and addtion where turned there would be an error
+
+
+//var example
+
+console.log(a)
+
+var a = 12
+
+//here the console.log() will be undifined 
