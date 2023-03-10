71ccfdd3eddd0a036d8a34afbeb611a661cb31f6
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-10 17:41:32 +0100

    update dom traversal

--- Roadmap js/Basic Dom/DOM Traversal.js
+++ Roadmap js/Basic Dom/DOM Traversal.js
@@ -27,7 +27,7 @@ const items = document.querySelectorAll(".item");
 /*
 nextSibling and previousSibling:
 The nextSibling property returns the next sibling node of an element, while the previousSibling property returns the previous sibling node of an element.
-*/ 
+*/
 
 const item = document.getElementById("myItem");
 const nextItem = item.nextSibling;
@@ -44,6 +44,61 @@ const prevItem = item.previousElementSibling;
 
 // just examples
 
-
 //WEB DEV
+
+//html
+
+<div id="grandParent">
+  <div class="parent">
+    <div class="children" id="child-one"></div>
+    <div class="children"></div>
+  </div>
+  <div class="parent">
+    <div class="children"></div>
+    <div class="children"></div>
+  </div>
+</div>;
+
+//get element by ID
+
+const grandparent = document.getElementById("grandParent");
+
+const parents = document.getElementsByClassName("parent");
+//now you get 2 elements
+
+//but you want them as a List of elements
+
+const parents = Array.from(document.getElementsByClassName("parent"));
+
+//Queryselector / QueryselectorAll
+
+const grandParent = document.querySelector("#grandParent") //like a css selector
+const parent = document.querySelector(".parent") 
+
+//querySelector just will get the first node that he finds
+
+const parents = document.querySelectorAll(".parent") //this returns a array of all nodes that have this css selector
+
+// Selecting children with querySelector
+
+const grandParent = document.querySelector(".grandparent")
+const parents = Array.from(grandParent.children)
+const parentOne = parents[0]
+const children = parentOne.children
+
+// queryselector dos not only work with the document 
+
+grandParent.querySelector(".children")
+
+//moving up the dome Tree
+
+const childOne = document.querySelector('#child-one')
+const parent = childOne.parentElement
+const grandParent = parent.parentElement
+
+const grandParent = childOne.closest(".grandparent") //closest is a reversed querySelecotor
+
+//previous and next element
+
+const childTwo = childOne.nextElementSibling
 
