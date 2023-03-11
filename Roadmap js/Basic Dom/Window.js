d08bf3a32cbc189590488e7cd8e8f1f3238ff405
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-11 19:50:49 +0100

    Window in JS

---
+++ Roadmap js/Basic Dom/Window.js
@@ -0,0 +1,20 @@
+// the window object
+
+/*
+In JavaScript, the DOM (Document Object Model) window is a global object that represents the browser window in which a web page is displayed. It provides a number of properties and methods that can be used to interact with the browser and the web page itself.
+*/
+
+/*
+Some common properties of the window object include:
+
+window.innerHeight and window.innerWidth: These properties return the height and width of the browser window, respectively.
+window.location: This property provides information about the URL of the web page, and can be used to navigate to a new page.
+window.document: This property provides access to the DOM of the current web page, allowing you to manipulate the content and structure of the page.
+window.alert(): This method displays an alert box with a message and an OK button, which can be used to provide feedback to the user.
+window.setTimeout(): This method allows you to execute a function after a specified amount of time has passed.
+window.addEventListener(): This method allows you to register event listeners on the window object, which can be used to respond to user input or other events.
+*/
+
+/*
+The window object is a fundamental part of JavaScript programming for web development, as it provides a way to interact with the browser and the web page itself. By using the properties and methods provided by the window object, you can create more dynamic and interactive web applications that respond to user input and provide feedback in real-time.
+*/
