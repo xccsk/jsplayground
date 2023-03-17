b5f58162c18a12f28c567ed5b475464b558cfb96
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-17 11:29:06 +0100

    Parcel Setup

---
+++ Roadmap js/Modules&Bundlers/Parcel.js
@@ -0,0 +1,11 @@
+//Parcel 
+
+/*
+it bundels your application and makes it ready to deploy it and it makes automatic transforms
+*/
+
+//Setup
+
+//npm init and do all the default
+
+//npm i --save-dev parcel-bundler
