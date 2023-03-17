6c98a70eb6a0255fefc01ae007a9b042abf25eb1
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-17 12:22:53 +0100

    npm

---
+++ Roadmap js/Modules&Bundlers/NPM.js
@@ -0,0 +1,22 @@
+//NPM
+
+/*
+NPM (Node Package Manager) is a package manager for JavaScript, primarily used in the Node.js ecosystem. It is a powerful tool that allows developers to easily manage, share, and distribute reusable code packages (known as modules) across projects. NPM simplifies the process of installing, updating, and removing these packages, making it easier to build and maintain complex applications.
+
+Here's a quick overview of some key concepts and features of NPM:
+*/
+
+/*
+Package: A package (or module) is a reusable piece of code or a library that can be easily shared and integrated into different projects. Packages can include anything from simple utility functions to full-featured frameworks.
+Package.json: This is the manifest file for a project, which contains metadata about the project, such as its name, version, author, and dependencies. NPM uses this file to understand which packages are required to build and run the project.
+Registry: The NPM registry is an online repository that hosts thousands of open-source packages. Developers can search for and download these packages using the NPM CLI (Command Line Interface) or by visiting the website at https://www.npmjs.com/.
+CLI (Command Line Interface): NPM includes a command-line tool that provides a simple interface to interact with the NPM registry. Some common NPM commands include npm install (to install packages), npm update (to update packages), and npm uninstall (to remove packages).
+Dependencies: These are the packages that a project relies on to function correctly. Dependencies are listed in the package.json file and can be installed using the npm install command.
+DevDependencies: These are packages required only for development purposes, such as testing or building tools. They are not needed for the project to run in production. DevDependencies are also listed in the package.json file and can be installed using the npm install --save-dev command.
+Versioning: NPM uses semantic versioning (or SemVer) to manage package versions. This system uses a three-part version number (major.minor.patch), making it easy to understand the type of changes made between different versions of a package.
+Node.js: NPM is tightly integrated with Node.js, a popular JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js allows developers to run JavaScript code outside of a web browser, enabling server-side development and other tasks that were not traditionally possible with JavaScript.
+*/
+
+/*
+In summary, NPM is a powerful tool that simplifies the process of managing and sharing code in the JavaScript ecosystem. It streamlines the installation, updating, and removal of packages, allowing developers to focus on writing code and building applications.
+*/
