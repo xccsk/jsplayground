07f986ccf222b4848c5518731f5696bf820834e3
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-20 17:56:27 +0100

    Knoten Punkte in JS

---
+++ jsbook/Webseiten dynamisch verändern/5.1/Knoten.js
@@ -0,0 +1,11 @@
+//Knoten
+
+/*
+im DOM Tree gibt es 12 verschiedene Knoten
+
+Dokumen Knoten (document.)
+Elementknoten (<main>,<h1>)
+Attributknoten (lang,id,summary)
+Textknoten (Text innerhalb von html knoten)
+
+*/
